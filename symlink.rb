0444e72ec456830f148124024c201caff3b6610a
root <root@024630db-6599-4cf9-9184-95964049dcaf>
2008-01-31 00:35:59 +0000

    create symlink script
    git-svn-id: file:///var/svn/repos/dotfiles@3 024630db-6599-4cf9-9184-95964049dcaf

---
+++ symlink.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/ruby
+require 'fileutils'
+
+HOME = ENV['HOME']
+files = Dir.glob('.*').reject {|f| %w(. .. .svn).include?(f) }
+FileUtils.rm_rf(files.map {|f| File.join(HOME, f) }, :verbose => true)
+FileUtils.symlink(files.map {|f| File.expand_path(f) }, HOME, :verbose => true)
