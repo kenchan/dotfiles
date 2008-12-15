3ca29fd299bc994e7ab7cde1615c7a26be4a62b8
kenchan <kenichi.taka@gmail.com>
2008-12-15 11:10:28 +0900

    Don't create ".git" symlink.

--- symlink.rb
+++ symlink.rb
@@ -2,6 +2,6 @@
 require 'fileutils'
 
 HOME = ENV['HOME']
-files = Dir.glob('.*').reject {|f| %w(. .. .svn).include?(f) }
+files = Dir.glob('.*').reject {|f| %w(. .. .svn .git).include?(f) }
 FileUtils.rm_rf(files.map {|f| File.join(HOME, f) }, :verbose => true)
 FileUtils.symlink(files.map {|f| File.expand_path(f) }, HOME, :verbose => true)
