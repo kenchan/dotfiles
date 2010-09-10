15a180c48e54e0f273327e5a381101a9c86ee4c9
Kenichi TAKAHASHI <kenichi.taka@gmail.com>
2010-09-10 12:08:37 +0900

    [awesome] change default layout

--- dot.config/awesome/rc.lua
+++ dot.config/awesome/rc.lua
@@ -46,7 +46,7 @@ layouts =
 tags = {}
 for s = 1, screen.count() do
     -- Each screen has its own tag table.
-    tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, s, layouts[1])
+    tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, s, layouts[2])
 end
 -- }}}
 
