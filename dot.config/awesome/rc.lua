5f6102f624c6a25eeb42a4e677118f8214680eae
Kenichi TAKAHASHI <kenichi.taka@gmail.com>
2011-01-21 17:46:48 +0900

    xrandrの後はawesomeの初期化が走るのでnitrogen --resotreはここでやる

--- dot.config/awesome/rc.lua
+++ dot.config/awesome/rc.lua
@@ -333,3 +333,4 @@ end)
 client.add_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.add_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+os.execute("nitrogen --restore &")
