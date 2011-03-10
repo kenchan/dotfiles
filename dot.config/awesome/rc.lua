684528d9bffe495a22a653764491067037781afd
Kenichi TAKAHASHI <kenichi.taka@gmail.com>
2011-03-10 12:08:22 +0900

    [awesome] change default terminal

--- dot.config/awesome/rc.lua
+++ dot.config/awesome/rc.lua
@@ -13,7 +13,7 @@ require("vicious")
 beautiful.init("/usr/share/awesome/themes/default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "sakura"
+terminal = "gnome-terminal"
 editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
 
