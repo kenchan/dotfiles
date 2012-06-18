4f77f8edea7642af0d5e709fd183c892366c7d15
Kenichi TAKAHASHI <kenichi.taka@gmail.com>
2012-06-18 11:49:07 +0900

    [awesome] always firefox run at No.9 tags.(for selenium)

--- dot.config/awesome/rc.lua
+++ dot.config/awesome/rc.lua
@@ -352,8 +352,8 @@ awful.rules.rules = {
     { rule = { class = "gimp" },
       properties = { floating = true } },
     -- Set Firefox to always map on tags number 2 of screen 1.
-    -- { rule = { class = "Firefox" },
-    --   properties = { tag = tags[1][2] } },
+    { rule = { class = "Firefox" },
+      properties = { tag = tags[1][9] } },
 }
 -- }}}
 
