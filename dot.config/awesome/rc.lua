c4e83aef45986e0465187ab76c6b1ab3b588b109
Kenichi TAKAHASHI <kenichi.taka@gmail.com>
2012-11-16 19:27:38 +0900

    run xcompmgr

--- dot.config/awesome/rc.lua
+++ dot.config/awesome/rc.lua
@@ -388,3 +388,4 @@ client.add_signal("focus", function(c) c.border_color = beautiful.border_focus e
 client.add_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
 awful.util.spawn_with_shell("/home/kenichi/bin/dex-once")
+awful.util.spawn_with_shell("xcompmgr")
