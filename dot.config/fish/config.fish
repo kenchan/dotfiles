e58ec461adf9d230e8f1fdc4e6c664c0b2563fd3
Kenichi Takahashi <kenichi.taka@gmail.com>
2018-02-18 18:23:23 +0900

    show full pwd in prompt

--- dot.config/fish/config.fish
+++ dot.config/fish/config.fish
@@ -5,6 +5,8 @@ set -gx GOPAHT $HOME
 
 set -gx PATH $GOPATH/bin $PATH
 
+set -g fish_prompt_pwd_dir_length 0
+
 function fish_user_key_bindings
   bind \cr 'peco_select_history'
   bind \co 'peco_select_ghq_repository'
