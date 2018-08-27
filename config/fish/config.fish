5e56cc2e22b91aa4d2c03b33be8883d88e8dc8f7
Kenichi Takahashi <kenichi.taka@gmail.com>
2018-08-27 17:22:23 +0900

    Use fish-ghq insted of fish-peco_select_ghq_repository
    fish-ghq includes ghq completions

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,6 +3,8 @@ alias g git
 alias less 'less -R'
 alias be 'bundle exec'
 
+set -U GHQ_SELECTOR peco
+
 set -gx GOPATH $HOME
 
 set -gx PATH $GOPATH/bin $PATH
@@ -17,7 +19,6 @@ set -g fish_prompt_pwd_dir_length 0
 
 function fish_user_key_bindings
   bind \cr 'peco_select_history'
-  bind \co 'peco_select_ghq_repository'
 end
 
 set -g fish_user_paths "/usr/local/opt/mysql@5.6/bin" $fish_user_paths
