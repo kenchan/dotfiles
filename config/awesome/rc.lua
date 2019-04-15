5548be049e57a18a26922322699ef00bcda992b7
Kenichi Takahashi <kenichi.taka@gmail.com>
2019-04-15 20:13:45 +0900

    [awesome] change theme

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -45,7 +45,7 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+beautiful.init(gears.filesystem.get_themes_dir() .. "zenburn/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "xterm"
