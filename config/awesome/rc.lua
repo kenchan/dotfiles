eb045e73e1d499470191425bf3dbdc94b31fe2a7
Kenichi Takahashi <kenichi.taka@gmail.com>
2019-04-15 20:07:56 +0900

    [awesome] change default layout to tile

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -61,7 +61,6 @@ modkey = "Mod4"
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
-    awful.layout.suit.floating,
     awful.layout.suit.tile,
     awful.layout.suit.tile.left,
     awful.layout.suit.tile.bottom,
@@ -74,6 +73,7 @@ awful.layout.layouts = {
     awful.layout.suit.max.fullscreen,
     awful.layout.suit.magnifier,
     awful.layout.suit.corner.nw,
+    awful.layout.suit.floating,
     -- awful.layout.suit.corner.ne,
     -- awful.layout.suit.corner.sw,
     -- awful.layout.suit.corner.se,
