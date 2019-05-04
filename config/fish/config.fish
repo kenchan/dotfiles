a15019ab065c697f5c117e1dba1e79122fced7e3
Kenichi Takahashi <kenichi.taka@gmail.com>
2019-05-04 23:19:41 +0900

    New version of fish-ghq adds default key-binding

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -27,10 +27,6 @@ eval (direnv hook fish)
 
 set -gx DOCKER_BUILDKIT 1
 
-function fish_user_key_bindings
-  bind \cg '__ghq_crtl_g'
-end
-
 if command -v direnv > /dev/null;
   eval (direnv hook fish)
 end
