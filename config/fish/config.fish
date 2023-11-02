f48e92e779090eee59b34c4a5bd468cc77911b22
Kenichi Takahashi <kenichi.taka@gmail.com>
2023-11-02 20:19:46 +0900

    add new abbr

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -24,6 +24,7 @@ abbr -a kx kubectx
 abbr -a ks kubens
 abbr -a tf terraform
 abbr -a rge rg -E euc-jp
+abbr -a vs "code (ghq list -p | peco)"
 
 if [ -d $HOME/.local/bin ]
   set -gx PATH ~/.local/bin $PATH
